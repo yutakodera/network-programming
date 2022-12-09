3b47743bf0b8cdf0e5312467c9e6d2edb4b9f0a2
yutakodera <47738291+yutakodera@users.noreply.github.com>
2022-12-09 22:55:53 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip 
+
+python3 -m pip install jupyterlab
+python3 -m pip install jupyter_http_over_ws
+jupyter serverxtension enable --py jupyter_http_over_ws
+jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0
+
